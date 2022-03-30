a6176a824bf9342868d692ace5ee78335b43017e
Pierce Gardner <pgardner@squadpod.com>
2022-03-30 16:37:54 -0700

    adds test class and tests

---
+++ .mocharc.js
@@ -0,0 +1,3 @@
+const { colors } = require('mocha/lib/reporters/base');
+colors.pass = 32;
+// symbols.ok = '😀';
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {TestFunc} from './testFunc';
+
+const testFunc = new TestFunc();
+
+console.info('The color is', testFunc.getColor);
+console.info(`The answer is ${testFunc.getNumber}`);
