43bdb66a54a91e3e37f5ba86d3cb12605d699a3c
Pierce Gardner <pgardner@squadpod.com>
2022-03-30 16:51:39 -0700

    Adds build scripts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import {TestFunc} from './testFunc';
 
 const testFunc = new TestFunc();
 
-console.info('The color is', testFunc.getColor);
-console.info(`The answer is ${testFunc.getNumber}`);
+console.info('The color is', testFunc.getColor());
+console.info(`The answer is ${testFunc.getNumber()}`);
